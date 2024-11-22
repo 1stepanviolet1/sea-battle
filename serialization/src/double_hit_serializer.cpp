275317f1039b9752b4c6a229852d0ae7cf357e71
Stepan Ivanov <stepanviolet@gmail.com>
2024-11-22 12:03:50 +0300

    fix taking the address of a temporary object DoubleHit's unit

--- serialization/src/double_hit_serializer.cpp
+++ serialization/src/double_hit_serializer.cpp
@@ -10,7 +10,8 @@ DoubleHitSerializer::operator()(Void *_obj)
     DoubleHit &double_hit = static_cast<DoubleHit&>(*_obj);
     
     UnitSerializer unit_serializer;
-    unit_serializer.save(&double_hit.unit());
+    Unit unit = double_hit.unit();
+    unit_serializer.save(&unit);
     this->_json["_unit"] = unit_serializer.get();
 
 }
